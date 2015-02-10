de8dd302b58741aae7de25bbb3fbc1ce686c7bd5
m8rge <to.merge@gmail.com>
2015-02-10 10:29:56 +0500

    captureException

--- src/SentryHelper.php
+++ src/SentryHelper.php
@@ -25,28 +25,35 @@ class SentryHelper
     /**
      * @param string $message
      * @param null|\Exception $previousException
+     * @param string $level one of Raven_Client::* levels
      * @param string $exceptionClass
      * @return bool
      * @throws \yii\base\InvalidConfigException
      */
-    public static function captureWithMessage($message, $previousException = null, $exceptionClass = 'yii\base\Exception')
+    public static function captureWithMessage($message, $previousException = null, $level = \Raven_Client::ERROR, $exceptionClass = 'yii\base\Exception')
     {
         /** @var ErrorHandler $raven */
         $raven = \Yii::$app->get('raven', false);
         if ($raven instanceof ErrorHandler) {
-            $raven->client->captureException(new $exceptionClass($message, 0, $previousException));
+            $raven->client->captureException(new $exceptionClass($message, 0, $previousException), ['level' => $level]);
             return true;
         }
 
         return false;
     }
 
-    public static function captureException($exception)
+    /**
+     * @param \Exception $exception
+     * @param string $level one of Raven_Client::* levels
+     * @return bool
+     * @throws \yii\base\InvalidConfigException
+     */
+    public static function captureException($exception, $level = \Raven_Client::ERROR)
     {
         /** @var ErrorHandler $raven */
         $raven = \Yii::$app->get('raven', false);
         if ($raven instanceof ErrorHandler) {
-            $raven->client->captureException($exception);
+            $raven->client->captureException($exception, ['level' => $level]);
             return true;
         }
 
